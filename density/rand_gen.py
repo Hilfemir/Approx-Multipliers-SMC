20f2a01a90f1f70e356b789ce02f3373e080ac75
Hilfemir <b.parak12@gmail.com>
2024-02-22 00:07:18 +0100

    random distributions of multiplication pairs in selected algorithms

--- density/rand_gen.py
+++ density/rand_gen.py
@@ -1,20 +1,63 @@
 import random
 import matplotlib.pyplot as plt
+from pyparsing import alphanums
 import seaborn as sns
 
 numbers = []
-alpha = 0.2
-beta = 3.0
 
 for i in range(10000):
-    num = random.gammavariate(alpha, beta)
-    num = int(num * 100)
-    numbers.append(num)
+	#Sieve of Pritchard - A
+	#alpha = 1.0
+	#beta = 0.8
+	#num = random.gammavariate(alpha, beta)
+	#num = int(num * 150)
+
+	#Sieve of Pritchard - B
+	#num = random.uniform(0, 2000)
+	#num = int(num)
+
+	#AKS Primality test - A
+	#num = random.triangular(-50, 450, 70)
+
+	#AKS Primality test - B
+	#alpha = 2.0
+	#beta = 2.0
+	#num = random.betavariate(alpha, beta)
+	#num = int(num * 400)
+
+	#Ellipse Midpoint - A
+	#alpha = 0.5
+	#beta = 5.0
+	#num = random.betavariate(alpha, beta)
+	#num = int(num * 40)
+
+	#Ellipse Midpoint - B
+	#mu = 9.0
+	#sigma = 15.0
+	#num = random.gauss(mu, sigma)
+
+	#Circle Point by Point - both A and B
+	#num = random.uniform(2, 100)
+
+	#Integer Square Root - both A and B
+	#num = random.triangular(-10, 35, 5)
+
+	#ElGamal Signature Scheme - A
+	#num = random.triangular(-20, 600, 20)
+
+	#ElGamal Signature Scheme - B
+	alpha = 1.7
+	beta = 1.7
+	num = random.weibullvariate(alpha, beta)
+	num = int(num * 200)
+
+	numbers.append(num)
 
 plt.figure(figsize=(10, 6))
-sns.kdeplot(numbers, bw_adjust=1.0)  # You can adjust the bandwidth (bw_adjust) for smoothing
-plt.title('Density Plot of Given Integers')
+sns.kdeplot(numbers)  # You can adjust the bandwidth (bw_adjust) for smoothing
+plt.title('ElGamal Signature Scheme - B')
 plt.xlabel('Value')
 plt.ylabel('Density')
 plt.grid(False)
+plt.savefig("./rand_generated/elgamal_b.png")
 plt.show()
