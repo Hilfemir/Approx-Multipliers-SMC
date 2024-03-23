aecc728120b7b8e4c141b3bbde14c24ccbb0365f
Hilfemir <b.parak12@gmail.com>
2024-03-23 01:43:54 +0100

    Modified process_results.py, created result folders for all distributions

--- results/process_results.py
+++ results/process_results.py
@@ -1,6 +1,8 @@
 import csv
 from pathlib import Path
 import pandas as pd
+import re
+import argparse
 
 class Result(object):
 	def __init__(self, dist: str, mult_id: str, metric: str, value: float):
@@ -16,44 +18,84 @@ class Result(object):
 		return (self.dist, self.mult_id, self.metric, self.value)
 
 
-def process_file(file_path) -> Result:
-	"""Open csv file, read the last row and collect all necessary info
-	for the Result object, then return it.
+def process_file(file_path) -> list[tuple]:
+	"""Open csv file and collect all necessary data.
+	Returns list of Result objects represented as tuples.
 	"""
+	#collect info about the multiplier and distribution
+	parts = file_path.parts
+	dist = parts[0]
+	mult_id = parts[1]
+
+	#collect lines from the csv file
 	data = []
 	with open(file_path, newline='') as f:
 		reader = csv.reader(f)
 		for row in reader:
 			data.append(row)
 
-	#collect info about the multiplier, distribution and error metric
-	parts = file_path.parts
-	dist = parts[0]
-	mult_id = parts[1]
-	metric = parts[2][:-4]
+	data = data[1:]
 
-	#last row is the one we are looking for
-	value = round(float(data[-1][1]), 2)
+	results = {
+		'coverage_percentage' : None,
+		'delay_avg' : None,
+		'error_prob' : None,
+		'mean_abs_error' : None,
+		'mean_relative_error' : None,
+		'mean_squared_error' : None,
+		'worst_case_error' : None,
+		'worst_case_relative_error' : None,
+		'hamming_distance' : None,
+		'worst_delay' : None,
+		'bit_flips' : None
+	}
 
-	ret = Result(dist, mult_id, metric, value)
+	#iterate through the csv lines and collect data for each error metric
+	title_pattern = r'#\s*(.+?)\s*#' #eg. '# mean_squared_error #1'
+	current_title = ""
+	for i, d in enumerate(data):
+		if title_match := re.search(title_pattern, d[0]):
+			if i > 0:
+				#found title of next metric, the prev item is the final val of the curr metric
+				results[current_title] = round(float(data[i-1][1]), 2)
+
+			current_title = title_match.group(1)
+
+		#last item
+		if i == len(data)-1:
+			results[current_title] = round(float(d[1]), 2)
+
+	#convert the results to Result objects (and their tuple representations)
+	ret = [Result(dist, mult_id, key, val).as_tuple() for key, val in results.items() if val is not None]
 	return ret
 	
 
 def main():
-	results = []
-	rootdir = Path('./uniform_uniform')
-	subdir_list = [d for d in rootdir.glob('**/*') if d.is_dir()]
+	parser = argparse.ArgumentParser(
+	                    prog='process_results.py',
+	                    description='Processes results from csv files, stores them in data.pkl'
+						)
+	
+	parser.add_argument(
+		'--dir', '-d', 
+		help='Directory containing csv files or subdirectories which contain csv files.',
+		default='',
+		required=False
+		)
+	
+	args = parser.parse_args()
 
-	#iterate through results from all multipliers
-	for dir in subdir_list:
-		#collect the csv files
-		file_list = [f for f in rootdir.glob('**/*') if f.is_file() and f.name.endswith(".csv")]
-		for file_path in file_list:
-			result = process_file(file_path)
-			results.append(result.as_tuple())
+	results = []
+	rootdir = Path(f'./{args.dir}')
+
+	#find all csv files in dir and all its subdirectories
+	file_list = [f for f in rootdir.glob('**/*') if f.is_file() and f.name.endswith(".csv")]
+	for file_path in file_list:
+		part_results = process_file(file_path)
+		results.extend(part_results)
 
 	df = pd.DataFrame(results, columns=["distribution", "multiplier", "metric", "value"])
-	print(df)
+	print(df.to_string())
 	
 
 if __name__ == "__main__":
