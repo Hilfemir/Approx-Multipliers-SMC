b67beefc050a93592d0eb119e73841557fad5ce7
Hilfemir <b.parak12@gmail.com>
2024-04-28 01:18:56 +0200

    memory scalability various multipliers

--- scalability/plot_time.py
+++ scalability/plot_time.py
@@ -10,6 +10,7 @@ Part of bachelor's thesis called Statistical model checking of approximate compu
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
 	df = pd.read_csv("./scalability.csv")
@@ -19,11 +20,15 @@ def main():
 	ax = fig.add_subplot(1,1,1)
 
 	new_df = df[df['Vstupní bity'] == '7x7']
-	new_df.plot(ax=ax, x="Počet hradel", y="Čas [s]", label="7x7", color="#7F58AF")
+	new_df.plot(ax=ax, x="Počet hradel", y="Čas [s]", label="mult_7x7", color="#7F58AF")
 
 	#filter out mults with same sized inputs
 	new_df = df[df['Vstupní bity'] == '8x8']
-	new_df.plot(ax=ax, x="Počet hradel", y="Čas [s]", label="8x8", color="#E84D8A")
+	new_df.plot(ax=ax, x="Počet hradel", y="Čas [s]", label="mult_8x8", color="#E84D8A")
+
+	x = np.arange(1, 351)
+	y = 4.3 * x
+	ax.plot(x, y, color="gray", label="y=4.3x", linestyle=":")
 
 	ax.set_ylabel("Čas [s]", fontsize=20)
 	ax.set_xlabel("Počet hradel", fontsize=20)
