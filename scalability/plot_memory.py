a163a76ed13dec6e8e2ac5787ed43b23032600a3
Hilfemir <b.parak12@gmail.com>
2024-04-28 09:50:31 +0200

    memory scalability plotting

--- scalability/plot_memory.py
+++ scalability/plot_memory.py
@@ -12,21 +12,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def main():
-	df = pd.read_csv("./scalability.csv")
+	df = pd.read_csv("./memory.csv")
 	plt.rc('legend',fontsize=16)
 
 	fig = plt.figure(figsize=(9,7))
 	ax = fig.add_subplot(1,1,1)
 
-	df.drop_duplicates(subset="Vstupní bity", inplace=True)
-
 	print(df)
+	df.plot(ax=ax, x="Vstupní bity", y="Paměť [GB]", color="#7F58AF")
 
-	ax.set_ylabel("Čas [s]", fontsize=20)
-	ax.set_xlabel("Počet hradel", fontsize=20)
+	ax.set_ylabel("Paměť [GB]", fontsize=20)
+	ax.set_xlabel("Vstupní bity", fontsize=20)
 	ax.tick_params(axis='both', which='major', labelsize=16)
+	ax.get_legend().remove()
 
-	#plt.show()
+	plt.show()
 
 if __name__ == "__main__":
 	main()
